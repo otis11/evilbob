253b913600a907bbb8d0d6f760e7f91cc9a4dceb
otis11 <148579276+otis11@users.noreply.github.com>
2024-10-05 20:46:24 +0200

    chore: add multiple shortcuts for windows/linux and mac

--- src/search-groups/shortcuts.ts
+++ src/search-groups/shortcuts.ts
@@ -10,16 +10,86 @@ export class SearchGroupShortcuts extends SearchGroup {
 		});
 	}
 
-	public getResults(): Promise<SearchResult[]> {
-		return new Promise((resolve) => {
-			resolve([
+	public async getResults(): Promise<SearchResult[]> {
+		const platform = (await new Promise((resolve) => {
+			chrome.runtime.getPlatformInfo(resolve);
+		})) as chrome.runtime.PlatformInfo;
+		const isMac = platform.os === "mac";
+
+		const shortcuts = [
+			new SearchResultShortcut({
+				title: "Open History",
+				description: "Open History",
+				shortcut: isMac
+					? new Shortcut(["⌘", "Y"])
+					: new Shortcut(["Ctrl", "H"]),
+			}),
+			// windows
+			new SearchResultShortcut({
+				title: "New Icognito Window",
+				description: "Open a new window in Incognito mode",
+				shortcut: isMac
+					? new Shortcut(["⌘", "Shift", "N"])
+					: new Shortcut(["Ctrl", "Shift", "N"]),
+			}),
+			// tabs
+			new SearchResultShortcut({
+				title: "New Tab",
+				description: "Open a new tab",
+				shortcut: isMac
+					? new Shortcut(["⌘", "T"])
+					: new Shortcut(["Ctrl", "T"]),
+			}),
+			new SearchResultShortcut({
+				title: "Close Tab",
+				description: "Close the current tab",
+				shortcut: isMac
+					? new Shortcut(["⌘", "W"])
+					: new Shortcut(["Ctrl", "W"]),
+			}),
+			new SearchResultShortcut({
+				title: "Reopen Tabs",
+				description:
+					"Reopen previously closed tabs in the order that they were closed",
+				shortcut: isMac
+					? new Shortcut(["⌘", "Shift", "T"])
+					: new Shortcut(["Ctrl", "Shift", "T"]),
+			}),
+			// bookmarks
+			new SearchResultShortcut({
+				title: "Save Bookmark",
+				description: "Save the current page as a bookmark",
+				shortcut: isMac
+					? new Shortcut(["⌘", "D"])
+					: new Shortcut(["Ctrl", "D"]),
+			}),
+			new SearchResultShortcut({
+				title: "Open Bookmarks",
+				description: "Open Bookmarks",
+				shortcut: isMac
+					? new Shortcut(["⌘", "Option", "B"])
+					: new Shortcut(["Ctrl", "Shift", "O"]),
+			}),
+			// search bar
+			new SearchResultShortcut({
+				title: "Focus Search",
+				description: "Put a cursor in the search bar",
+				shortcut: isMac
+					? new Shortcut(["Ctrl", "I"])
+					: new Shortcut(["Ctrl", "L"]),
+			}),
+			// other
+		];
+		if (!isMac) {
+			shortcuts.push(
 				new SearchResultShortcut({
-					title: "Open Bookmarks",
-					description: "Open Bookmarks",
-					shortcut: new Shortcut(["Ctrl", "Shift", "O"]),
+					title: "Toggle Fullscreen",
+					description: "Turn on/off full-screen mode",
+					shortcut: new Shortcut(["F11"]),
 				}),
-			]);
-		});
+			);
+		}
+		return shortcuts;
 	}
 }
 
@@ -34,7 +104,7 @@ export class SearchResultShortcut extends SearchResult {
 		super({
 			title: config.title,
 			description: config.description,
-			searchText: `${config.title} config.description`,
+			searchText: `${config.title} ${config.description}`,
 			icon: iconConsole,
 			shortcut: config.shortcut,
 		});

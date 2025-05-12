37785bd65bf39c4210735481bed30e589b12de8f
otis11 <hi-otis11@pm.me>
2025-05-12 21:22:18 +0200

    chore: improve keybinding inputs

--- src/views/options/App.tsx
+++ src/views/options/App.tsx
@@ -1,22 +1,27 @@
 import { Button } from "@/components/ui/button.tsx";
-import { ExternalLinkIcon } from "lucide-react";
+import { ExternalLinkIcon, Loader2 } from "lucide-react";
 import { type ChangeEvent, useEffect, useRef, useState } from "react";
 
 import { KeybindingInput } from "@/components/KeybindingInput";
 import { NumberSelect } from "@/components/NumberSelect.tsx";
 import { Input } from "@/components/ui/input.tsx";
-import { type EvilbobConfig, updateConfig } from "@/lib/config.ts";
-import type { Keybinding, KeybindingKey } from "@/lib/keybindings.ts";
+import { type EvilbobConfig, getConfig, updateConfig } from "@/lib/config.ts";
+import {
+	type Keybinding,
+	type KeybindingKey,
+	defaultKeybindings,
+} from "@/lib/keybindings.ts";
 
-interface AppProps {
-	config: EvilbobConfig;
-}
-export default function App({ config }: AppProps) {
+export default function App() {
+	const [localConfig, setLocalConfig] = useState<EvilbobConfig | undefined>(
+		undefined,
+	);
 	const [searchValue, setSearchValue] = useState("");
 	const inputRef = useRef<HTMLInputElement>(null);
 	const [keybindingLoading, setKeybindingLoading] = useState<string>("");
 
 	useEffect(() => {
+		getConfig().then((config) => setLocalConfig(config));
 		inputRef.current?.focus();
 	}, []);
 
@@ -43,104 +48,144 @@ export default function App({ config }: AppProps) {
 				},
 			},
 		});
+		setLocalConfig(await getConfig());
 		setKeybindingLoading("");
 	}
 
 	async function onHeightChange(value: number) {
+		if (!localConfig) {
+			return;
+		}
 		await updateConfig({
 			dimensions: {
 				height: value,
-				width: config.dimensions.width,
+				width: localConfig.dimensions.width,
 			},
 		});
+		setLocalConfig(await getConfig());
 	}
 
 	async function onWidthChange(value: number) {
+		if (!localConfig) {
+			return;
+		}
 		await updateConfig({
 			dimensions: {
 				width: value,
-				height: config.dimensions.height,
+				height: localConfig.dimensions.height,
 			},
 		});
+		setLocalConfig(await getConfig());
+	}
+
+	async function resetKeybindings() {
+		await updateConfig({
+			keybindings: defaultKeybindings,
+		});
+		setLocalConfig(await getConfig());
 	}
 
 	return (
 		<>
-			<div className="flex gap-4 items-center mb-4">
-				<Input
-					autoCapitalize="off"
-					autoCorrect="off"
-					autoComplete="off"
-					ref={inputRef}
-					className="h-12 !text-lg"
-					onChange={onChange}
-					data-vlist-stay-focused
-				></Input>
-				<Button onClick={onPluginsClick}>
-					<span className="mr-2">Plugins</span>
-					<ExternalLinkIcon></ExternalLinkIcon>
-				</Button>
-			</div>
-			<h2 className="font-bold text-base">Keybindings</h2>
-			{(Object.keys(config.keybindings) as KeybindingKey[])
-				.filter((key) => {
-					return (
-						config.keybindings[key].keys
-							.join("")
-							.toLowerCase()
-							.includes(searchValue.toLowerCase()) ||
-						config.keybindings[key].title
-							?.toLowerCase()
-							.includes(searchValue.toLowerCase()) ||
-						config.keybindings[key].description
-							?.toLowerCase()
-							.includes(searchValue.toLowerCase())
-					);
-				})
-				.map((key) => (
-					<div
-						className={
-							keybindingLoading === key
-								? "opacity-60 pointer-events-none"
-								: ""
-						}
-						key={config.keybindings[key]?.title}
-					>
-						<h3 className="text-sm pt-4">
-							{config.keybindings[key]?.title}
-						</h3>
-						<span className="text-muted-foreground text-sm">
-							{config.keybindings[key]?.description}
-						</span>
-						<KeybindingInput
-							className="max-w-60"
-							keys={config.keybindings[key]?.keys || []}
-							onAllKeysUp={(keys) =>
-								onKeysUpInput(
-									key,
-									config.keybindings[key],
-									keys,
-								)
-							}
-						/>
+			{localConfig ? (
+				<>
+					<div className="flex gap-4 items-center mb-4">
+						<Input
+							autoCapitalize="off"
+							autoCorrect="off"
+							autoComplete="off"
+							ref={inputRef}
+							className="h-12 !text-lg"
+							onChange={onChange}
+							data-vlist-stay-focused
+						></Input>
+						<Button onClick={onPluginsClick}>
+							<span className="mr-2">Plugins</span>
+							<ExternalLinkIcon></ExternalLinkIcon>
+						</Button>
 					</div>
-				))}
-			<h2 className="font-bold text-base pt-4">Window Dimensions</h2>
-			<div className="flex gap-4 items-center">
-				<NumberSelect
-					container={document.body}
-					values={[400, 500, 600, 800, 1000]}
-					onValueChange={onHeightChange}
-					value={config.dimensions.height}
-				></NumberSelect>
-				<NumberSelect
-					container={document.body}
-					values={[400, 500, 600, 800, 1000]}
-					onValueChange={onWidthChange}
-					value={config.dimensions.width}
-				></NumberSelect>
-			</div>
-			<div className="pb-12"></div>
+					<div className="flex gap-4 items-center">
+						<h2 className="font-bold text-base">Keybindings</h2>
+						<Button onClick={resetKeybindings} variant="ghost">
+							Reset
+						</Button>
+					</div>
+
+					{(Object.keys(localConfig.keybindings) as KeybindingKey[])
+						.filter((key) => {
+							return (
+								localConfig.keybindings[key].keys
+									.join("")
+									.toLowerCase()
+									.includes(searchValue.toLowerCase()) ||
+								localConfig.keybindings[key].title
+									?.toLowerCase()
+									.includes(searchValue.toLowerCase()) ||
+								localConfig.keybindings[key].description
+									?.toLowerCase()
+									.includes(searchValue.toLowerCase())
+							);
+						})
+						.map((key) => (
+							<div
+								className={`${
+									keybindingLoading === key
+										? "opacity-60 pointer-events-none"
+										: ""
+								} relative`}
+								key={localConfig.keybindings[key]?.title}
+							>
+								{keybindingLoading === key ? (
+									<Loader2
+										className="animate-spin absolute bottom-2 left-2"
+										size={20}
+									/>
+								) : (
+									""
+								)}
+								<h3 className="text-sm pt-4">
+									{localConfig.keybindings[key]?.title}
+								</h3>
+								<span className="text-muted-foreground text-sm">
+									{localConfig.keybindings[key]?.description}
+								</span>
+								<KeybindingInput
+									className="max-w-60"
+									keys={
+										localConfig.keybindings[key]?.keys || []
+									}
+									onKeydownTimeout={(keys) =>
+										onKeysUpInput(
+											key,
+											localConfig.keybindings[key],
+											keys,
+										)
+									}
+								/>
+							</div>
+						))}
+					<h2 className="font-bold text-base pt-4">
+						Window Dimensions (width, height)
+					</h2>
+					<div className="flex gap-4 items-center">
+						<NumberSelect
+							container={document.body}
+							values={[400, 500, 600, 800, 1000]}
+							onValueChange={onWidthChange}
+							value={localConfig.dimensions.width}
+						></NumberSelect>
+						<NumberSelect
+							container={document.body}
+							values={[400, 500, 600, 800, 1000]}
+							onValueChange={onHeightChange}
+							value={localConfig.dimensions.height}
+						></NumberSelect>
+					</div>
+					<div className="pb-12"></div>
+				</>
+			) : (
+				""
+			)}
 		</>
 	);
 }
--- src/views/options/main.tsx
+++ src/views/options/main.tsx
@@ -3,17 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "../../content-script.ts";
 import "../../globals.css";
-import { getConfig } from "@/lib/config.ts";
 import { automaticThemeUpdateDocument } from "@/lib/theme-preference.ts";
 
 automaticThemeUpdateDocument();
-getConfig().then((config) => {
-	const root = document.getElementById("root");
-	if (root) {
-		createRoot(root).render(
-			<StrictMode>
-				<App config={config} />
-			</StrictMode>,
-		);
-	}
-});
+const root = document.getElementById("root");
+if (root) {
+	createRoot(root).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}

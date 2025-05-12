296b5fc52036e0f557e41284c16074f20fc101f1
otis11 <hi-otis11@pm.me>
2025-05-12 20:08:04 +0200

    chore: reload bookmarks after editing or removing a bookmark. improve bookmark current tab and remove bookmark current tab commands

--- src/plugins/bookmarks/bookmark-current-tab.tsx
+++ src/plugins/bookmarks/bookmark-current-tab.tsx
@@ -2,6 +2,13 @@ import { toast } from "@/components/Toast.tsx";
 import { browserApi } from "@/lib/browser-api.ts";
 
 export async function Command() {
+	const [tabBookmark] = await browserApi.bookmarks.search({
+		url: window.location.href,
+	});
+	if (tabBookmark) {
+		toast("Tab already bookmarked.");
+		return;
+	}
 	await browserApi.bookmarks.create({
 		url: window.location.href,
 		title: document.title,
--- src/plugins/bookmarks/remove-bookmark-current-tab.tsx
+++ src/plugins/bookmarks/remove-bookmark-current-tab.tsx
@@ -1,13 +1,9 @@
 import { toast } from "@/components/Toast.tsx";
-import { browserApi, getCurrentTab } from "@/lib/browser-api.ts";
+import { browserApi } from "@/lib/browser-api.ts";
 
 export async function Command() {
-	const currentTab = await getCurrentTab();
-	if (!currentTab) {
-		return;
-	}
 	const [tabBookmark] = await browserApi.bookmarks.search({
-		url: currentTab.url,
+		url: window.location.href,
 	});
 	if (!tabBookmark) {
 		return;
